7498df4f2a0b34e1e1b88e9cbe134b8bbdad5f8a
Arshpreet Singh Sidhu <ARSHPREETMALKANA@gmail.com>
2025-09-22 16:38:09 -0700

    test

--- Backend/src/routes/user.js
+++ Backend/src/routes/user.js
@@ -2,10 +2,46 @@
 import userController from '../controllers/userController.js'
 
 export default async function (fastify, opts) {
-  fastify.get('/', userController.getUsers)
-  fastify.post('/', {
+  fastify.get('/', {
     schema: {
-      body: { $ref: 'userSchema#' }
+      description: 'Get all users',
+      tags: ['Users'],
+      response: {
+        200: {
+          type: 'array',
+          items: { $ref: 'userSchema#' }
+        }
+      }
     }
-  }, userController.createUser)
+  }, userController.getUsers)
+  fastify.post(
+    '/',
+    {
+      schema: {
+        description: 'Create a new user',
+        tags: ['Users'],
+        body: { $ref: 'userSchema#' },   // request validation
+        response: {
+          201: {                         // response on success
+            type: 'object',
+            properties: {
+              id: { type: 'number' },
+              name: { type: 'string' },
+              age: { type: 'number' }
+            }
+          },
+          400: {                         // validation error (Fastify auto)
+            type: 'object',
+            properties: {
+              statusCode: { type: 'number' },
+              error: { type: 'string' },
+              message: { type: 'string' }
+            }
+          }
+        }
+      }
+    },
+    userController.createUser
+  )
+
 }
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,11 +1,29 @@
-// src/server.js
 import userSchema from './schemas/userSchema.js'
 
 export default async function (fastify, opts) {
   // Load schemas
   fastify.addSchema(userSchema)
 
-  // Register plugins
-  await fastify.register(import('./plugins/cors.js'))
+  // Swagger generator
+  await fastify.register(import('@fastify/swagger'), {
+    openapi: {
+      info: {
+        title: 'AH Punjab Backend',
+        description: 'Fastify + OpenAPI demo',
+        version: '1.0.0'
+      }
+    }
+  })
+
+  // Swagger UI
+  await fastify.register(import('@fastify/swagger-ui'), {
+    routePrefix: '/docs',
+    uiConfig: {
+      docExpansion: 'list',
+      deepLinking: false
+    }
+  })
+
+  // Routes
   await fastify.register(import('./routes/user.js'), { prefix: '/users' })
 }
